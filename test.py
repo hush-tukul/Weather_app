a95077d3bea4c158d4b62943abe443d56ddb123e
hush-tukul <hush-tukul@protonmail.com>
2023-12-11 21:31:43 +0100

    Initial commit

--- test.py
+++ test.py
@@ -1,84 +1,97 @@
-import datetime
+# Імпортування необхідних бібліотек
 import json
 import threading
 import time
-
 import requests
 import schedule
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
 
+# Завантаження змінних середовища
+load_dotenv()
 
+# Ініціалізація Flask-додатку та планувальника на фоновому режимі
 app = Flask(__name__)
 scheduler = BackgroundScheduler()
 scheduler.start()
 
-
+# Встановлення URL API та необхідних параметрів
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
-
-API_KEY = 'ca385afe9e09204fa0ebfa0b4f968f12'
-X_TOKEN = 'x_token'
+API_KEY = os.getenv('API_KEY')
+X_TOKEN = os.getenv('X_TOKEN')
 CITY = 'Kyiv'
 
-
+# Функція для отримання даних про погоду
 def get_weather():
-    params = {
-        'q': CITY,
-        'appid': API_KEY
-    }
+  params = {
+      'q': CITY,
+      'appid': API_KEY
+  }
 
-    headers = {
-        'x-token': X_TOKEN
-    }
+  headers = {
+      'x-token': X_TOKEN
+  }
 
-    response = requests.get(BASE_URL, params=params, headers=headers)
+  # Відправка GET-запиту до API
+  response = requests.get(BASE_URL, params=params, headers=headers)
 
-    if response.status_code == 200:
-        weather_data = response.json()
+  # Перевірка успішності запиту
+  if response.status_code == 200:
+      weather_data = response.json()
 
-        # Add timestamp to the weather data
-        timestamp = int(time.time())  # Get current timestamp
-        weather_data['timestamp'] = timestamp  # Add timestamp to the data
+      # Додавання часу до даних про погоду
+      timestamp = int(time.time()) # Отримання поточного часу
+      weather_data['timestamp'] = timestamp # Додавання часу до даних
 
-        try:
-            with open('weather_data.json', 'r') as file:
-                data = json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            data = []
+      # Завантаження існуючих даних про погоду
+      try:
+          with open('weather_data.json', 'r') as file:
+              data = json.load(file)
+      except (FileNotFoundError, json.JSONDecodeError):
+          data = []
 
-        # Ensure 'data' is a list, even if it starts empty or from previous JSON data
-        if not isinstance(data, list):
-            data = []
+      # Переконання, що 'data' є списком, навіть якщо він починається порожнім або з попередніх даних JSON
+      if not isinstance(data, list):
+          data = []
 
-        data.append(weather_data)
+      # Додавання нових даних про погоду до списку
+      data.append(weather_data)
 
-        with open('weather_data.json', 'w') as file:
-            json.dump(data, file)
-    else:
-        print('Error: Weather data not available')
+      # Збереження оновлених даних про погоду
+      with open('weather_data.json', 'w') as file:
+          json.dump(data, file)
+  else:
+      print('Error: Weather data not available')
 
+# Функція для виконання запланованих завдань
 def update_weather():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+  while True:
+      schedule.run_pending()
+      time.sleep(1)
 
+# Маршрут Flask для отримання даних про погоду
 @app.route('/weather')
 def retrieve_weather():
-    try:
-        with open('weather_data.json', 'r') as file:
-            weather = json.load(file)
-            return jsonify(weather)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return jsonify({'error': 'Weather data not available'}), 404
+  try:
+      with open('weather_data.json', 'r') as file:
+          weather = json.load(file)
+          return jsonify(weather)
+  except (FileNotFoundError, json.JSONDecodeError):
+      return jsonify({'error': 'Weather data not available'}), 404
 
+# Головний блок виконання
 if __name__ == '__main__':
-    get_weather()  # Get weather data initially
-    schedule.every(1).minutes.do(get_weather)  # Schedule to get weather data every 5 minutes
+  get_weather() # Отримання даних про погоду спочатку
+  schedule.every().hour.do(get_weather) # Запланувати отримання даних про погоду кожну годину
 
-    update_thread = threading.Thread(target=update_weather)
-    update_thread.start()
+  # Запуск нового потоку для виконання планувальника
+  update_thread = threading.Thread(target=update_weather)
+  update_thread.start()
 
-    app.run()
+  # Запуск Flask-додатку
+  app.run()
 
 
 
--- test_2.py
+++ test_2.py
@@ -1,66 +1,61 @@
-import json
-
+# Імпортування необхідних бібліотек
 import requests
 from datetime import datetime as dt
 import os
+from dotenv import load_dotenv
 
-
+# Завантаження змінних середовища
+load_dotenv()
 BASE_URL = 'http://127.0.0.1:5000/weather'
-X_TOKEN = 'x_token'  # Replace 'your_x_token_here' with your actual x_token
+X_TOKEN = os.getenv('X_TOKEN')
 
-
+# Функція для перетворення температури з Кельвіна в Цельсію
 def kelvin_to_celsius(temp_kelvin):
-    return temp_kelvin - 273.15  # Conversion from Kelvin to Celsius
+   return temp_kelvin - 273.15
 
-
+# Функція для отримання температури для певної дати
 def get_temperature_for_date(date):
-    params = {'day': date}  # Update 'day' instead of 'date' for the query parameter
-    headers = {'x-token': X_TOKEN}
-    response = requests.get(BASE_URL, params=params, headers=headers)
+   params = {'day': date}
+   headers = {'x-token': X_TOKEN}
+   response = requests.get(BASE_URL, params=params, headers=headers)
 
-    if response.status_code == 200:
-        weather_data = response.json()
+   # Перевірка успішності запиту
+   if response.status_code == 200:
+       weather_data = response.json()
 
-        print(f"Temperature data for {date}:")
+       print(f"Temperature data for {date}:")
 
-        if isinstance(weather_data, list):
-            for data_point in weather_data:
-                timestamp = data_point.get('timestamp')
-                temperature_kelvin = data_point.get('main', {}).get('temp')
+       # Перевірка, чи є дані списком
+       if isinstance(weather_data, list):
+           for data_point in weather_data:
+               timestamp = data_point.get('timestamp')
+               temperature_kelvin = data_point.get('main', {}).get('temp')
 
-                if timestamp is not None and temperature_kelvin is not None:
-                    # Convert timestamp to human-readable date
-                    timestamp_human_readable = dt.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+               # Перевірка, чи є в даних час і температура
+               if timestamp is not None and temperature_kelvin is not None:
+                  timestamp_human_readable = dt.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+                  temperature_celsius = kelvin_to_celsius(temperature_kelvin)
+                  print(f"Time: {timestamp_human_readable}, Temperature: {temperature_celsius:.2f} °C")
+               else:
+                  print("Timestamp or temperature not found in the data.")
+       else:
+           timestamp = weather_data.get('timestamp')
+           temperature_kelvin = weather_data.get('main', {}).get('temp')
 
-                    # Convert temperature from Kelvin to Celsius
-                    temperature_celsius = kelvin_to_celsius(temperature_kelvin)
+           # Перевірка, чи є в даних час і температура
+           if timestamp is not None and temperature_kelvin is not None:
+               timestamp_human_readable = dt.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+               temperature_celsius = kelvin_to_celsius(temperature_kelvin)
+               print(f"Time: {timestamp_human_readable}, Temperature: {temperature_celsius:.2f} °C")
+           else:
+               print("Timestamp or temperature not found in the data.")
+   else:
+       print("Error:", response.json())
 
-                    print(
-                        f"Time: {timestamp_human_readable}, Temperature: {temperature_celsius:.2f} °C")  # Print formatted data
-                else:
-                    print("Timestamp or temperature not found in the data.")
-        else:
-            timestamp = weather_data.get('timestamp')
-            temperature_kelvin = weather_data.get('main', {}).get('temp')
-
-            if timestamp is not None and temperature_kelvin is not None:
-                # Convert timestamp to human-readable date
-                timestamp_human_readable = dt.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-
-                # Convert temperature from Kelvin to Celsius
-                temperature_celsius = kelvin_to_celsius(temperature_kelvin)
-
-                print(
-                    f"Time: {timestamp_human_readable}, Temperature: {temperature_celsius:.2f} °C")  # Print formatted data
-            else:
-                print("Timestamp or temperature not found in the data.")
-    else:
-        print("Error:", response.json())
-
-# Replace 'desired_date' with the date for which you want weather data in 'YYYY-MM-DD' format
-desired_date = '2023-12-11'  # For example, fetching weather for December 11, 2023
-
+# Використання функції для отримання температури для певної дати
+desired_date = '2023-12-11'
 get_temperature_for_date(desired_date)
+
 
 
 
